12ebc2ad4d1a66e84371eeaf406eb2f90351e0e8
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-18 20:53:52 +0800

    Updated imports

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
-import App from './App';
+import AppContainer from './containers/AppContainer';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <AppContainer />
   </Provider>,
   document.getElementById('root')
 );
