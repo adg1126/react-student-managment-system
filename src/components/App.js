1d69f0c7758e1c6e58425fe9c2d5016546cde2d0
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-18 12:12:07 +0800

    Integrated redux and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,28 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from '../ui/Theme';
+
+import NavBarContainer from '../containers/NavBarContainer';
 import Dashboard from '../pages/Dashboard';
 import Attendance from '../pages/Attendance';
 import Classes from '../pages/Classes';
 import Assignments from '../pages/Assignments';
 import Schedule from '../pages/Schedule';
-import NavBar from './NavBar/NavBar';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <NavBar>
-        <Switch>
-          <Route exact path='/' component={Dashboard} />
-          <Route path='/attendance' component={Attendance} />
-          <Route path='/classes' component={Classes} />
-          <Route path='/assignments' component={Assignments} />
-          <Route path='/schedule' component={Schedule} />
-        </Switch>
-      </NavBar>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <NavBarContainer>
+          <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/attendance' component={Attendance} />
+            <Route path='/classes' component={Classes} />
+            <Route path='/assignments' component={Assignments} />
+            <Route path='/schedule' component={Schedule} />
+          </Switch>
+        </NavBarContainer>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
