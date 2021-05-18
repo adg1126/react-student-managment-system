7e8fc9167aec929cb731153735f7eb2038d9b0e5
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-18 21:04:36 +0800

    Code cleanup

--- src/redux/classes/classesSelectors.js
+++ src/redux/classes/classesSelectors.js
@@ -11,18 +11,3 @@ export const selectClasseListForPreview = createSelector(
   [selectClasseList],
   classes => (classes ? Object.keys(classes).map(key => classes[key]) : [])
 );
-
-// export const selectCollection = collectionId =>
-//   createSelector([selectShopCollections], collections =>
-//     collections ? collections[collectionId] : null
-//   );
-
-// export const selectIsCollectionFetching = createSelector(
-//   [selectShop],
-//   shop => shop.isFetching
-// );
-
-// export const selectIsCollectionLoaded = createSelector(
-//   [selectShop],
-//   shop => !!!shop.collections
-// );
