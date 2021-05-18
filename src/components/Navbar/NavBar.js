f9fcbd6d6d0095a1b4024169c5b4dcc6f3fa42a6
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-18 12:08:08 +0800

    Styled and refactored

---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import clsx from 'clsx';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRighttIcon from '@material-ui/icons/ChevronRight';
+
+import NavBarItemsContainer from '../../containers/NavBarItemsContainer';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap'
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.common.white,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerClose: {
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.common.white,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.down('sm')]: {
+      width: theme.spacing(7) + 1
+    }
+  },
+  icon: {
+    fill: 'rgba(255, 255, 255, .8)'
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3)
+  }
+}));
+
+const NavBar = ({ children, drawerOpen, setDrawerOpen }) => {
+  const classes = useStyles();
+
+  return (
+    <Grid container>
+      <Grid item>
+        <Drawer
+          variant='permanent'
+          className={clsx(classes.drawer, {
+            [classes.drawerOpen]: drawerOpen,
+            [classes.drawerClose]: !drawerOpen
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: drawerOpen,
+              [classes.drawerClose]: !drawerOpen
+            })
+          }}
+        >
+          <Divider />
+          <NavBarItemsContainer />
+          <div
+            className={classes.toolbar}
+            style={{ justifyContent: drawerOpen ? 'flex-end' : 'center' }}
+          >
+            <IconButton onClick={() => setDrawerOpen(!drawerOpen)}>
+              {drawerOpen ? (
+                <ChevronLeftIcon className={classes.icon} />
+              ) : (
+                <ChevronRighttIcon className={classes.icon} />
+              )}
+            </IconButton>
+          </div>
+        </Drawer>
+      </Grid>
+      <Grid item>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          {children}
+        </main>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default NavBar;
---
+++ src/components/Navbar/NavBarItems.js
@@ -0,0 +1,148 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { Typography } from '@material-ui/core';
+
+import PieChartIcon from '@material-ui/icons/PieChart';
+import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
+import ClassIcon from '@material-ui/icons/Class';
+import ScheduleIcon from '@material-ui/icons/Schedule';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+
+const useStyles = makeStyles(theme => ({
+  drawerItemSelected: {
+    color: theme.palette.common.highlightedIndigo
+  }
+}));
+
+const NavBarItems = ({ drawerOpen, tabIndex, setTabIndex }) => {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const ROUTES = [
+    {
+      section: 'Overview',
+      routes: [
+        {
+          name: 'Dashboard',
+          icon: () => (
+            <PieChartIcon
+              style={{
+                fill:
+                  tabIndex === 0
+                    ? theme.palette.common.highlightedIndigo
+                    : 'white'
+              }}
+            />
+          ),
+          route: '/',
+          index: 0
+        }
+      ]
+    },
+    {
+      section: 'Manage',
+      routes: [
+        {
+          name: 'Attendance',
+          icon: () => (
+            <PlaylistAddCheckIcon
+              style={{
+                fill:
+                  tabIndex === 1
+                    ? theme.palette.common.highlightedIndigo
+                    : 'white'
+              }}
+            />
+          ),
+          route: '/attendance',
+          index: 1
+        },
+        {
+          name: 'Classes',
+          icon: () => (
+            <ClassIcon
+              style={{
+                fill:
+                  tabIndex === 2
+                    ? theme.palette.common.highlightedIndigo
+                    : 'white'
+              }}
+            />
+          ),
+          route: '/classes',
+          index: 2
+        },
+        {
+          name: 'Assignments',
+          icon: () => (
+            <AssignmentIndIcon
+              style={{
+                fill:
+                  tabIndex === 3
+                    ? theme.palette.common.highlightedIndigo
+                    : 'white'
+              }}
+            />
+          ),
+          route: '/assignments',
+          index: 3
+        },
+        {
+          name: 'Schedule',
+          icon: () => (
+            <ScheduleIcon
+              style={{
+                fill:
+                  tabIndex === 4
+                    ? theme.palette.common.highlightedIndigo
+                    : 'white'
+              }}
+            />
+          ),
+          route: '/schedule',
+          index: 4
+        }
+      ]
+    }
+  ];
+
+  return ROUTES.map((section, i) => (
+    <List key={i}>
+      {drawerOpen ? (
+        <Typography
+          style={{
+            padding: '1em',
+            fontSize: 15,
+            fontWeight: 500
+          }}
+        >
+          {section.section}
+        </Typography>
+      ) : null}
+      {section.routes.map(({ name, icon, route, index }) => (
+        <ListItem
+          button
+          component={Link}
+          to={route}
+          key={name}
+          onClick={() => {
+            setTabIndex(index);
+          }}
+          selected={tabIndex === index}
+          classes={{ selected: classes.drawerItemSelected }}
+        >
+          <ListItemIcon>{icon()}</ListItemIcon>
+          <ListItemText style={{ fontSize: 14 }}>{name}</ListItemText>
+        </ListItem>
+      ))}
+    </List>
+  ));
+};
+
+export default NavBarItems;
