765825f0cff817aad202d4f5db7e3e10591bfd52
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-19 20:57:38 +0800

    Removed unecessary selectors

--- src/redux/classes/classesSelectors.js
+++ src/redux/classes/classesSelectors.js
@@ -2,12 +2,7 @@ import { createSelector } from 'reselect';
 
 const selectClasses = state => state.classes;
 
-export const selectClasseList = createSelector(
+export const selectClassList = createSelector(
   [selectClasses],
   classes => classes.classList
-);
-
-export const selectClasseListForPreview = createSelector(
-  [selectClasseList],
-  classes => (classes ? Object.keys(classes).map(key => classes[key]) : [])
 );
