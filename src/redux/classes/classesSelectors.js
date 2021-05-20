26637f15cec71f9229a19ede0da8cb405aaafbfa
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-20 10:20:42 +0800

    Memoized fetching selector

--- src/redux/classes/classesSelectors.js
+++ src/redux/classes/classesSelectors.js
@@ -2,7 +2,12 @@ import { createSelector } from 'reselect';
 
 const selectClasses = state => state.classes;
 
-export const selectClassList = createSelector(
+export const selectClassesClassList = createSelector(
   [selectClasses],
   classes => classes.classList
+);
+
+export const selectIsClassesFetching = createSelector(
+  [selectClasses],
+  classes => classes.isFetching
 );
