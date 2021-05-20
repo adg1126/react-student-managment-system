856bc836130ded5cf588365307fda918deecddcb
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-20 19:20:18 +0800

    Wired up Redux Rorm

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import tabsReducer from './tabs/tabsReducer';
 import classesReducer from './classes/classesReducer';
 
-export default combineReducers({ tabs: tabsReducer, classes: classesReducer });
+export default combineReducers({
+  tabs: tabsReducer,
+  classes: classesReducer,
+  form: formReducer
+});
