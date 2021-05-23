9782e0ece9e6dcaac360dc367805fc0241cebf02
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-23 09:14:49 +0800

    Replaced redux thunk with redux-saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,20 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+// import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(sagaMiddleware))
 );
+
+sagaMiddleware.run(rootSaga);
 
 export { store };
