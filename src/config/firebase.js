9dcb23fa7ec83a66c33db6d3c6a4aeec50456e9f
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-26 13:59:24 +0800

    Setup google auth utility

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -23,5 +23,10 @@ export const getClassRef = async courseCode => {
 };
 
 export const firestore = firebase.firestore();
+export const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
