5742fab0bb02c4c07a2e5efb091e60e23965c832
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-27 00:15:43 +0800

    Code that adds users to user collection in firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -13,6 +13,32 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return userRef;
+};
+
 export const getClassRef = async courseCode => {
   const classesRef = firestore
     .collection('classes')
