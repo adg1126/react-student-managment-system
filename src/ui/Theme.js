8380803a47de01c2fdeb0b82d893357af33542f4
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-26 13:19:58 +0800

    Updated theme

--- src/ui/Theme.js
+++ src/ui/Theme.js
@@ -4,10 +4,10 @@ import createBreakpoints from '@material-ui/core/styles/createBreakpoints';
 const customBreakpointValues = {
   values: {
     xs: 0,
-    sm: 576,
-    md: 768,
-    lg: 992,
-    xl: 1200
+    sm: 600,
+    md: 960,
+    lg: 1280,
+    xl: 1920
   }
 };
 
