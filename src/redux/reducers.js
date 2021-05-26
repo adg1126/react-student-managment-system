bccf8e88d0582d7c6627d7071016d8e154890bc8
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-27 00:20:57 +0800

    Wired up userReducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,10 +3,12 @@ import { reducer as formReducer } from 'redux-form';
 import navbarReducer from './navbar/navbarReducer';
 import classesReducer from './classes/classesReducer';
 import notificationReducer from './notification/notificationReducer';
+import userReducer from './user/userReducer';
 
 export default combineReducers({
   navbar: navbarReducer,
   classes: classesReducer,
   notification: notificationReducer,
+  user: userReducer,
   form: formReducer
 });
