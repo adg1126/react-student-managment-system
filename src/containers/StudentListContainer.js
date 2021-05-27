c777af34a105385bb4c92eb07276bfd2f06dc551
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-27 21:35:23 +0800

    Dynamically pass props based on route

--- src/containers/StudentListContainer.js
+++ src/containers/StudentListContainer.js
@@ -1,7 +1,32 @@
+import { compose } from 'redux';
 import { connect } from 'react-redux';
+import {
+  selectStudentListForPreview,
+  selectIsStudentsFetching
+} from '../redux/student/studentSelectors';
 
-import { deleteStudent } from '../redux/classes/classesActions';
+import { deleteStudent } from '../redux/student/studentActions';
+import history from '../history';
 
 import StudentList from '../components/students/StudentList';
+import WithSpinner from './WithSpinner';
 
-export default connect(null, { deleteStudent })(StudentList);
+const mapStateToProps = state => {
+  const path = history.location.pathname;
+  const classId = path.substring(path.lastIndexOf('/') + 1);
+  const studentsArr = selectStudentListForPreview(state);
+
+  return {
+    studentList: classId
+      ? studentsArr.filter(student =>
+          student.courses.every(course => course.includes(classId))
+        )
+      : studentsArr,
+    isFetching: selectIsStudentsFetching(state)
+  };
+};
+
+export default compose(
+  connect(mapStateToProps, { deleteStudent }),
+  WithSpinner
+)(StudentList);
