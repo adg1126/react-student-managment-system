1863d8d278cad8ea1706b1b75b52a1e4a51029f9
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-27 15:12:36 +0800

    Improved styling

--- src/components/classes/ClassListCard.js
+++ src/components/classes/ClassListCard.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import history from '../../history';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
@@ -17,17 +17,25 @@ const useStyles = makeStyles(theme => ({
   mainContainer: {
     width: '100%'
   },
+  cardContainer: {
+    border: `1px solid ${theme.palette.common.grey600}`,
+    padding: '0.5em 0'
+  },
+  cardContent: {
+    cursor: 'pointer',
+    '&:hover': {
+      color: 'blue'
+    }
+  },
   redButton: {
     ...theme.button,
     ...theme.buttonRedAnimation,
-    fontSize: '1em',
-    marginBottom: '1.5em'
+    fontSize: '1em'
   },
   indigoButton: {
     ...theme.button,
     ...theme.buttonIndigoAnimation,
-    fontSize: '1em',
-    marginBottom: '1.5em'
+    fontSize: '1em'
   },
   linkButton: {
     color: theme.palette.secondary.main,
@@ -46,14 +54,16 @@ const ClassCard = ({ docId, courseCode, courseName, deleteClass }) => {
   const classes = useStyles();
 
   return (
-    <Card>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant='body1' component='h2'>
-            {`${courseCode} - ${courseName}`}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+    <Card className={classes.cardContainer}>
+      <CardContent className={classes.cardContent}>
+        <Typography
+          variant='body1'
+          component='h2'
+          onClick={() => history.push(`/classes/${docId}`)}
+        >
+          {`${courseCode} - ${courseName}`}
+        </Typography>
+      </CardContent>
       <CardActions>
         <Button
           variant='outlined'
