5e37e0bad54ca0831bacce3c6a331b68a1853d04
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-27 21:27:43 +0800

    Wired up student sagas

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,7 +1,8 @@
 import { all, call } from 'redux-saga/effects';
 import { classesSagas } from './classes/classSagas';
 import { userSagas } from './user/userSagas';
+import { studentSagas } from './student/studentSagas';
 
 export default function* rootSaga() {
-  yield all([call(classesSagas), call(userSagas)]);
+  yield all([call(classesSagas), call(userSagas), call(studentSagas)]);
 }
