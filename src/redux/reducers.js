cfede7a389e2407d6b1c16582ebd4215e4a719dd
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-27 21:28:07 +0800

    Wired up student reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,11 +4,13 @@ import navbarReducer from './navbar/navbarReducer';
 import classesReducer from './classes/classesReducer';
 import notificationReducer from './notification/notificationReducer';
 import userReducer from './user/userReducer';
+import studentReducer from './student/studentReducers';
 
 export default combineReducers({
   navbar: navbarReducer,
   classes: classesReducer,
   notification: notificationReducer,
   user: userReducer,
+  student: studentReducer,
   form: formReducer
 });
