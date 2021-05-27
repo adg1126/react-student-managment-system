1a9954f2a4aa92921eb2c4e5930492ede2c87b89
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-27 14:37:16 +0800

    SwitchGroup reusable component

---
+++ src/components/SwitchGroup.js
@@ -0,0 +1,31 @@
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+
+const SwitchGroup = ({ options, input }) => {
+  const switchGroup = () =>
+    options.map((option, i) => (
+      <FormControlLabel
+        key={i}
+        control={
+          <Switch
+            name={`${input.name}[${i}]`}
+            value={option.name}
+            checked={input.value.indexOf(option.name) !== -1}
+            onChange={event => {
+              const newValue = [...input.value];
+
+              event.target.checked
+                ? newValue.push(option.name)
+                : newValue.splice(newValue.indexOf(option.name), 1);
+
+              return input.onChange(newValue);
+            }}
+          />
+        }
+        label={option.name}
+      />
+    ));
+  return <div>{switchGroup()}</div>;
+};
+
+export default SwitchGroup;
