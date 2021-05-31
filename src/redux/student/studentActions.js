ae24b3c7e658431111d30d712cafd08266013ad3
Diane998 <gutierrez.almiradyan@gmail.com>
2021-05-31 14:07:38 +0800

    action creators caffolding

--- src/redux/student/studentActions.js
+++ src/redux/student/studentActions.js
@@ -2,7 +2,9 @@ import {
   FETCH_STUDENTS_START,
   ADD_STUDENT_START,
   DELETE_STUDENT_START,
-  EDIT_STUDENT_START
+  EDIT_STUDENT_START,
+  ADD_EXISTING_STUDENT_TO_COURSE,
+  DELETE_STUDENT_FROM_COURSE
 } from './studentActionTypes';
 
 export const fetchStudentsStart = () => ({
@@ -23,4 +25,14 @@ export const editStudent = (docId, studentObj) => ({
   type: EDIT_STUDENT_START,
   key: docId,
   value: studentObj
+});
+
+export const addExistingStudentToCourse = studentObj => ({
+  type: ADD_EXISTING_STUDENT_TO_COURSE,
+  payload: studentObj
+});
+
+export const deleteStudentFromCourse = studentObj => ({
+  type: DELETE_STUDENT_FROM_COURSE,
+  payload: studentObj
 });
