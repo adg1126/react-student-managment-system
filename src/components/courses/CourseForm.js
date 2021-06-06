74a58eea3be346a3120a70766e0ba2787d92ac45
Diane998 <gutierrez.almiradyan@gmail.com>
2021-06-06 21:11:26 +0800

    Updated SwitchGroup and CourseForm

--- src/components/courses/CourseForm.js
+++ src/components/courses/CourseForm.js
@@ -1,37 +1,18 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { Field, reduxForm, formValueSelector } from 'redux-form';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormLabel from '@material-ui/core/FormLabel';
-import SwitchGroup from '../SwitchGroup';
+import SwitchGroup from '../fields/SwitchGroup';
 
 const useStyles = makeStyles(theme => ({
   textField: {
-    '& label.Mui-focused': {
-      color: 'blue'
-    },
-    '& .MuiInput-underline:after': {
-      borderBottomColor: 'blue'
-    },
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: theme.palette.common.grey800
-      },
-      '&:hover fieldset': {
-        borderColor: theme.palette.common.grey800
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: 'blue'
-      }
-    },
-    margin: '0.5em 0'
-  },
-  datePicker: {
-    '& label': { color: 'rgba(255,255,255,0.1)' },
     '& label.Mui-focused': {
       color: 'blue'
     },
@@ -81,31 +62,73 @@ const renderTextField = ({
     {...input}
     {...custom}
     margin='dense'
-    fullWidth
   />
 );
 
-const renderDatePicker = ({
-  label,
-  input,
-  meta: { touched, invalid, error },
-  ...custom
-}) => (
-  <TextField
-    label={label}
-    placeholder={label}
-    error={touched && invalid}
-    helperText={touched && error}
-    {...input}
-    {...custom}
-    margin='dense'
-  />
-);
-
-const CourseForm = props => {
+const DaysMeetAndTime = ({ daysMeet, input }) => {
   const classes = useStyles();
 
-  const { pristine, reset, setModalOpen, modalName } = props;
+  return [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday'
+  ].map((day, i) => (
+    <Grid
+      key={i}
+      style={{
+        visibility: daysMeet && daysMeet.includes(day) ? 'visible' : 'hidden'
+      }}
+      container
+      direction='row'
+      spacing={3}
+      justify='center'
+    >
+      <Grid item>
+        <Field
+          className={classes.textField}
+          variant='outlined'
+          name={`${input.name}.${day}.startTime`}
+          component={renderTextField}
+          label='Start Time'
+          type='time'
+          InputLabelProps={{
+            shrink: true
+          }}
+          inputProps={{
+            step: 300 // 5 min
+          }}
+        />
+      </Grid>
+      <Grid item>
+        <Field
+          className={classes.textField}
+          variant='outlined'
+          name={`${input.name}.${day}.endTime`}
+          component={renderTextField}
+          label='End Time'
+          type='time'
+          InputLabelProps={{
+            shrink: true
+          }}
+          inputProps={{
+            step: 300 // 5 min
+          }}
+        />
+      </Grid>
+    </Grid>
+  ));
+};
+
+let CourseForm = props => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const matchesXS = useMediaQuery(theme.breakpoints.down('xs'));
+
+  const { pristine, reset, setModalOpen, modalName, daysMeet } = props;
 
   const onSubmit = formValues => {
     props.onSubmit(formValues);
@@ -120,6 +143,7 @@ const CourseForm = props => {
         name='courseCode'
         component={renderTextField}
         label='Course Code'
+        fullWidth
       />
       <Field
         className={classes.textField}
@@ -127,42 +151,62 @@ const CourseForm = props => {
         name='courseName'
         component={renderTextField}
         label='Course Name'
+        fullWidth
       />
       <Field
         style={{ marginRight: '2em' }}
-        className={classes.datePicker}
+        className={classes.textField}
         variant='outlined'
-        name='startDate'
-        component={renderDatePicker}
+        name='courseDates.startDate'
+        component={renderTextField}
         label='Start Date'
         type='date'
+        InputLabelProps={{
+          shrink: true
+        }}
       />
       <Field
-        className={classes.datePicker}
+        className={classes.textField}
         variant='outlined'
-        name='endDate'
-        component={renderDatePicker}
+        name='courseDates.endDate'
+        component={renderTextField}
         label='End Date'
         type='date'
+        InputLabelProps={{
+          shrink: true
+        }}
       />
-      <FormGroup style={{ margin: '1em 0' }}>
-        <FormLabel style={{ color: 'black', margin: '0.5em 0' }}>
-          Meeting Days
-        </FormLabel>
-        <Field
-          name='meetingDays'
-          component={SwitchGroup}
-          options={[
-            { id: 1, name: 'monday' },
-            { id: 2, name: 'tuesday' },
-            { id: 3, name: 'wednesday' },
-            { id: 4, name: 'thursday' },
-            { id: 5, name: 'friday' },
-            { id: 6, name: 'saturday' },
-            { id: 7, name: 'sunday' }
-          ]}
-        />
-      </FormGroup>
+      {matchesXS ? null : (
+        <Grid container direction='row' justify='space-between'>
+          <Grid item container style={{ width: '28%' }}>
+            <FormGroup style={{ margin: '1em 0' }}>
+              <FormLabel style={{ color: 'black', margin: '0.5em 0' }}>
+                Meeting Days
+              </FormLabel>
+              <Field
+                name='daysMeet'
+                component={SwitchGroup}
+                options={[
+                  { id: 1, name: 'monday' },
+                  { id: 2, name: 'tuesday' },
+                  { id: 3, name: 'wednesday' },
+                  { id: 4, name: 'thursday' },
+                  { id: 5, name: 'friday' },
+                  { id: 6, name: 'saturday' },
+                  { id: 7, name: 'sunday' }
+                ]}
+              />
+            </FormGroup>
+          </Grid>
+          <Grid item container style={{ width: '72%', marginTop: '4em' }}>
+            <Field
+              name='daysMeetAndTime'
+              component={DaysMeetAndTime}
+              daysMeet={daysMeet}
+            />
+          </Grid>
+        </Grid>
+      )}
       <Grid container className={classes.buttonContainer}>
         <Grid item>
           {setModalOpen ? (
@@ -195,15 +239,29 @@ const CourseForm = props => {
 const validate = values => {
   const errors = {};
 
-  const requiredFields = ['courseCode', 'courseName', 'startDate', 'endDate'];
+  const requiredFields = ['courseCode', 'courseName', 'courseDates'];
   requiredFields.forEach(field => {
-    if (!values[field]) errors[field] = 'Required';
+    if (field !== 'courseDates') {
+      if (!values[field]) errors[field] = 'Required';
+    } else {
+      errors[field] = {};
+      if (!values[field]) errors[field]['startDate'] = 'Required';
+      if (!values[field]) errors[field]['endDate'] = 'Required';
+    }
   });
 
   return errors;
 };
 
-export default reduxForm({
-  form: 'courseForm',
-  validate
-})(CourseForm);
+const selector = formValueSelector('courseForm');
+
+const mapStateToProps = state => ({
+  daysMeet: selector(state, 'daysMeet')
+});
+
+export default connect(mapStateToProps)(
+  reduxForm({
+    form: 'courseForm',
+    validate
+  })(CourseForm)
+);
