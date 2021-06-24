53afb69304c591f258529812d77fc38ac4b68a85
Diane998 <gutierrez.almiradyan@gmail.com>
2021-06-24 10:45:40 +0800

    Wired up setCurrentDate action and memoized selectors

--- src/containers/attendance/AttendanceContainer.js
+++ src/containers/attendance/AttendanceContainer.js
@@ -2,34 +2,24 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 
 import { createStructuredSelector } from 'reselect';
-import { selectCurrentCourse } from '../../redux/courses/coursesSelectors';
 import {
   selectCurrentCourseDates,
   selectIsAttendanceFetching,
   selectAttendanceStatus
 } from '../../redux/attendance/attendanceSelectors';
 
-import {
-  fetchAttendanceStart,
-  setCurrentCourse,
-  setCurrentDate
-} from '../../redux/attendance/attendanceActions';
+import { setCurrentDate } from '../../redux/attendance/attendanceActions';
 
 import Attendance from '../../pages/Attendance';
 import WithSpinner from '../WithSpinner';
 
 const mapStateToProps = createStructuredSelector({
   isFetching: selectIsAttendanceFetching,
-  currentCourse: selectCurrentCourse,
-  CourseDates: selectCurrentCourseDates,
+  courseDates: selectCurrentCourseDates,
   status: selectAttendanceStatus
 });
 
 export default compose(
-  connect(mapStateToProps, {
-    fetchAttendanceStart,
-    setCurrentCourse,
-    setCurrentDate
-  }),
+  connect(mapStateToProps, { setCurrentDate }),
   WithSpinner
 )(Attendance);
